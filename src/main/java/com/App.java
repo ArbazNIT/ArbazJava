bae236ca73b57f67cfa08460fe36005f20dc191f
Arbaz Khan <arbazfkhan01@gmail.com>
2024-05-21 01:54:30 +0530

    print somthing in main method

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("App.main()");
+       System.out.println("App.main()");
+       
     }
 }
